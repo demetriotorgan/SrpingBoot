280950ec8434979d1ba0c34ed533fdbc88dd1e44
demetriotorgan <demetriotorgan@hotmail.com>
2024-11-12 23:35:04 -0300

    Domain model

--- src/main/java/com/torgan/aula1/services/GameService.java
+++ src/main/java/com/torgan/aula1/services/GameService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.torgan.aula1.dto.GameMinDTO;
 import com.torgan.aula1.entities.Game;
@@ -15,10 +14,11 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-   @Transactional(readOnly = true)
+   
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(GameMinDTO::new).toList();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
 	}
    
 }
